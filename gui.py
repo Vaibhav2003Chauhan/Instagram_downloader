cf7b2e0d41b651132129b08c37140053eadef7ca
vaibhav chauhan <vishuchauhan461@gmail.com>
2023-10-02 20:58:18 +0530

    started

--- gui.py
+++ gui.py
@@ -5,5 +5,9 @@ root.geometry('1000x800')
 root.maxsize(1000,800)
 root.minsize(1000,800)
 
+txt1=Label(root,text="Instagram downloader",font="Helvetica 40 ",bg="cyan")
+txt1.pack(fill='x')
+
+
 
 root.mainloop()
