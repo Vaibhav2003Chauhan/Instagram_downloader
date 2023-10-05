c040c1cbd02c2ded195bc93c49a95bae13ec6d01
vaibhav chauhan <vishuchauhan461@gmail.com>
2023-10-05 12:01:39 +0530

    updating

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This was an python project used to download out all post and profile pic from instagram
+python gui.py
--- gui.py
+++ gui.py
@@ -1,4 +1,10 @@
 from tkinter import *
+# function for downloading up the  post or link 
+def download():
+    print("HELLO")
+
+
+
 root=Tk()
 root.title('Instagram Detail Fetcher')
 root.geometry('1000x800')
@@ -7,8 +13,18 @@ root.minsize(1000,800)
 
 heading1=Label(root,text="Instagram downloader",font=" Courie 40 bold ",bg="cyan")
 heading1.pack(fill='x')
-heading2=Label(root,text="By Vaibhav Chauhan", font="Times 20 ", bg="red" ,fg="black")
+heading2=Label(root,text="By Vaibhav Chauhan", font="Times 30 ", bg="red" ,fg="black")
 heading2.pack(fill="x")
+user_input=StringVar()
+f1=Frame(root)
+f1.pack()
+user_giveninput=Entry(f1, textvariable=user_input,width=100)
+
+user_heading=Label(f1,font=('Times,15,itaic'),text="enter username or Link")
+user_heading.grid(row=0, column=1)
+user_giveninput.grid(row=1,column=2)
+b1=Button(f1,text="download",relief=SUNKEN, command=download)
+b1.grid(row=1, column=1)
 
 
 
