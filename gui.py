c404b634be999de6a50097e1ab836020ff163307
vaibhav chauhan <vishuchauhan461@gmail.com>
2023-10-06 19:55:57 +0530

    added status bar and initialize functions

--- gui.py
+++ gui.py
@@ -2,30 +2,33 @@ from tkinter import *
 # function for downloading up the  post or link 
 def download():
     print("HELLO")
+    var=user_input.get()
+    print(f" the user input is {var}")
 
 
 
 root=Tk()
 root.title('Instagram Detail Fetcher')
-root.geometry('1000x800')
-root.maxsize(1000,800)
-root.minsize(1000,800)
-
-heading1=Label(root,text="Instagram downloader",font=" Courie 40 bold ",bg="cyan")
+root.geometry('900x800')
+# root.maxsize(900,800)
+root.minsize(900,800)
+# main heading 
+heading1=Label(root,text="Instagram downloader",font=" Courie 38 bold ",bg="cyan")
 heading1.pack(fill='x')
-heading2=Label(root,text="By Vaibhav Chauhan", font="Times 30 ", bg="red" ,fg="black")
-heading2.pack(fill="x")
+
 user_input=StringVar()
 f1=Frame(root)
-f1.pack()
-user_giveninput=Entry(f1, textvariable=user_input,width=100)
+f1.pack(padx=10,pady=20)
+user_giveninput=Entry(f1, textvariable=user_input,width=80)
 
-user_heading=Label(f1,font=('Times,15,itaic'),text="enter username or Link")
-user_heading.grid(row=0, column=1)
-user_giveninput.grid(row=1,column=2)
+user_heading=Label(f1,font=('Times,15,itaic'),text="Enter username ")
+user_heading.grid(row=0, column=3)
+user_giveninput.grid(row=3,column=3)
 b1=Button(f1,text="download",relief=SUNKEN, command=download)
-b1.grid(row=1, column=1)
-
+b1.grid(row=4, column=3)
+# status bar at bottom 
+data_label=Label(text="status shows here", bg='cyan', fg='black',font='Times 22 ',height=2)
+data_label.pack(side=BOTTOM, fill='x')
 
 
 root.mainloop()
